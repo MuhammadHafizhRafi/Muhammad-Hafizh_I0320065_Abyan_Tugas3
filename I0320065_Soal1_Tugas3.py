a18406c3690f4a7b213837df751bd856e55774de
Hafizh Rafi <hafizhrs06@gmail.com>
2021-03-19 23:35:04 +0700

    yes

---
+++ I0320065_Soal1_Tugas3.py
@@ -0,0 +1,20 @@
+# List 10 list_teman
+list_teman = ['Hasan', 'Fagih', 'Memet', 'Rapli', 'Hilmy', 'Sasa', 'Odrey', 'Rizal', 'Iffa', 'Hanna']
+
+# Menampilkan list indeks nomor 4,6, dan 7
+print("List indeks nomor 4,6, dan 7 adalah", list_teman[4], ",", list_teman[6], ", dan", list_teman[7])
+
+# Mengganti nama list_teman di list 3,5, dan 9
+list_teman[3] = 'Alya'
+list_teman[5] = 'Nanda'
+list_teman[9] = 'Attar'
+
+# Menambahkan 2 nama list_teman
+list_teman.append('Ayu')
+list_teman.append('Candrika')
+
+# Menampilkan semua list_teman dengan pengulangan
+print(list_teman * 2)
+
+# Menampilkan panjang list
+print(len(list_teman))
---
+++ I0320065_Soal2_Tugas3.py
@@ -0,0 +1,9 @@
+# Membuat dictionary
+dict = {'Nama':'Hafizh','Hobi1':'Bermain bola','Hobi2':'Main musik','Hobi3':'Beradventure','Sosmed1':'ig:@hafizhrafi','Sosmed2':'twitter:@pipisdisokin','Sosmed3':'Facebook:@muhammadhafizh','Lagu1':'Anarchy in the UK','Lagu2':'Bulan&ksatria','Lagu3':'Arjuna','Makanan1':'Sambel teri','Makanan2':'Nasi goreng','Makanan3':'Sate'}
+
+# Mengubah salah satu hobi dan sosmed, hapus 2 makanan, dan tambah 1 hobi
+dict['Hobi3'] = 'Ngisengin orang'
+dict['Sosmed3'] = 'snapchat:@hafizhrafiii'
+del dict['Makanan3']
+del dict['Makanan2']
+dict['Hobi4'] = 'Baca Quran'
